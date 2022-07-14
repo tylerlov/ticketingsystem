ee21688844902c6502df81305661569a0562c3b6
stylotylo <tylerlovell@gmail.com>
2022-07-14 12:41:01 -0400

    Debugging Ticket Controller

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -8,7 +8,16 @@ const Ticket = require('../models/ticketModel');
 // @access Private
 
 const getTickets = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'getTickets'})
+    //Get user using id and JWT
+    const user = await User.findById(req.user.id)
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const tickets = await Ticket.find({ user: req.user._id })
+
+    res.status(200).json({tickets})
 })
 
 // @desc   Create a ticket
@@ -16,7 +25,28 @@ const getTickets = asyncHandler(async (req, res) => {
 // @access Private
 
 const createTicket = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'createTicket'})
+    const { product, description } = req.body;
+
+    if(!product || !description){
+        res.status(400)
+        throw new Error('Please provide a product and description')
+    }
+
+    //Get user using id and JWT
+    const user = await User.findById(req.user.id)
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user._id,
+        status: 'new'
+    })
+
+    res.status(201).json(ticket)
 })
 
 module.exports = {
